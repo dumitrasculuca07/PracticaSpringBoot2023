12a327765ca3f9d82347729b1069fab836fd2652
Nuuume <da@email.com>
2023-07-10 18:49:52 +0300

    Tema basic completed

--- src/main/java/com/example/PracticaSpringBoot2023/controller/ControllerClubSportiv.java
+++ src/main/java/com/example/PracticaSpringBoot2023/controller/ControllerClubSportiv.java
@@ -21,16 +21,24 @@ public class ControllerClubSportiv {
     public static List<ClubSportiv> getCluburi(){
 
         List<Jucatori> listaJucatori1 = new ArrayList<>();
-        Jucatori j1 = new Jucatori(1,"Luca",21,3);
-        Jucatori j2 = new Jucatori(2,"Marius",30,5);
-        Jucatori j3 = new Jucatori(3,"Octavian",21,2);
-        listaJucatori1 = List.of(j1,j2,j3); // Liverpool
+        Jucatori j11 = new Jucatori(1,"Luca",21,3);
+        Jucatori j12 = new Jucatori(2,"Marius",30,5);
+        Jucatori j13 = new Jucatori(3,"Octavian",21,2);
+        listaJucatori1 = List.of(j11,j12,j13); // Liverpool
 
         List<Jucatori> listaJucatori2 = new ArrayList<>();
+        Jucatori j21 = new Jucatori(1,"Ederson",28,5);
+        Jucatori j22 = new Jucatori(2,"Ake",24,2);
+        Jucatori j23 = new Jucatori(3,"Foden",23,4);
+        listaJucatori2 = List.of(j21,j22,j23); // ManCity
 
         List<Jucatori> listaJucatori3 = new ArrayList<>();
+        Jucatori j31 = new Jucatori(1,"Saliba",22,1);
+        Jucatori j32 = new Jucatori(2,"Havertz",24,1);
+        Jucatori j33 = new Jucatori(3,"Jesus",26,2);
+        listaJucatori3 = List.of(j31,j32,j33); // Arsenal
 
-        ClubSportiv c1 = new ClubSportiv(1,5,"Liverpool","130 ani",20,listaJucatori1);
+        ClubSportiv c1 = new ClubSportiv(1,5,"Liverpool","130 ani",50,listaJucatori1);
         ClubSportiv c2 = new ClubSportiv(2,5,"Manchester City","125 ani",30,listaJucatori2);
         ClubSportiv c3 = new ClubSportiv(3,5,"Arsenal","135 ani",35,listaJucatori3);
 
